8295c1345dacbfee188fca4efbfa10660ad6d89b
LeulYonas <108304956+LeulYonas@users.noreply.github.com>
2022-09-25 14:12:44 -0700

    practice 1

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@
 console.log("I am Yonas");
 
 console.log("My favorite food is Banana");
+
+
+const string = "Supercalifragilisticexpialidocious";
+console.log(string.length);
+
+console.log (5);
+console.log (typeof 5);
+
+console.log (5n);
+console.log (typeof 5n);
+
